99751c64c531615fabfc438aa140a738c48c6728
Ilyass Kamal <125494269+Ueudje@users.noreply.github.com>
2024-02-11 14:09:26 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const list = require('./100-data').list;
-
-console.log(list);
-const New = list.map((x, i) => x * i);
-console.log(New);
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  const NewList = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    NewList.push(list[i]);
-  }
-  return NewList;
-};
