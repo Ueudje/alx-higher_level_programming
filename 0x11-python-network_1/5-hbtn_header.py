bfcc6eda1e4b18307d58c69efe403d988d1de011
Ilyass Kamal <125494269+Ueudje@users.noreply.github.com>
2024-02-11 14:07:59 +0100

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/5-hbtn_header.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""import module"""
-
-if __name__ == "__main__":
-    import requests
-    from sys import argv
-    print(requests.get(argv[1]).headers.get("X-Request-Id"))
--- 0x11-python-network_1/6-post_email.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""import model"""
-
-if __name__ == "__main__":
-    import requests
-    from sys import argv
-    print(requests.post(argv[1], data={'email': argv[2]}).text)
